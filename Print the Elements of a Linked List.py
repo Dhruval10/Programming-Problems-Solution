d542081d1f22734cced377890228ae6957f6d02b
Dhruval <22280539+Dhruval10@users.noreply.github.com>
2020-08-27 18:39:37 -0700

    Create Print the Elements of a Linked List.py

---
+++ Print the Elements of a Linked List.py
@@ -0,0 +1,16 @@
+def printLinkedList(head):
+    current = head
+    while current:
+        print(current.data)
+        current = current.next
+
+if __name__ == '__main__':
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    printLinkedList(llist.head)
