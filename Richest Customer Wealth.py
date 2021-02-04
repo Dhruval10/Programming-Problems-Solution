bc18589c3ca7bbb15dab756e07a90f05107d1d2e
Dhruval <22280539+Dhruval10@users.noreply.github.com>
2021-02-03 23:33:03 -0800

    Add files via upload

--- Richest Customer Wealth.py
+++ Richest Customer Wealth.py
@@ -1,11 +1,11 @@
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
+        
         maxi = 0
         
-        for arr in accounts:
-            sum1 = 0
-            for i in arr:
-                sum1 += i
-            if sum1 > maxi:
-                maxi = sum1
+        for account in accounts:
+            temp_sum = sum(account)
+            if temp_sum > maxi:
+                maxi = temp_sum
+        
         return maxi
