be2faa17b547dfbcc101da727ab635b1b3b7866c
Dhruval <22280539+Dhruval10@users.noreply.github.com>
2021-03-03 23:00:45 -0800

    Create Contains Duplicate.py

---
+++ Mar 3/Contains Duplicate.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        nums.sort()
+        
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+            else:
+                pass
+        return False
