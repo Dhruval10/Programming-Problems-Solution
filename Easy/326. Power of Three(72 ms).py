4bb14a1cad5d5e9d01a6e0f28c1076053b77919a
Dhruval <22280539+Dhruval10@users.noreply.github.com>
2021-05-09 23:10:06 -0400

    Create 326. Power of Three(72 ms).py

---
+++ Easy/326. Power of Three(72 ms).py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        
+        if n <= 0:
+            return False
+        
+        while n % 3 == 0:
+            n /= 3
+        
+        return True if n == 1 else False
