27611e9d5114a23a24ba6f72eb39b0bebb3f9553
Dhruval <22280539+Dhruval10@users.noreply.github.com>
2021-08-13 23:27:21 -0700

    Create 141. Linked List Cycle.py

---
+++ Easy/141. Linked List Cycle.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        visited = set()
+        
+        traverse = head
+        while traverse:
+            if traverse in visited:
+                return True
+            visited.add(traverse)
+            traverse = traverse.next
+        return False
